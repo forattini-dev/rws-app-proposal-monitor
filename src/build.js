84dd60267e016f6458bea9fe6f4bb69d9a2318a5
Filipe Forattini <filipe@vitta.me>
2024-03-07 12:30:06 -0300

    fix: build multi env

--- src/build.js
+++ src/build.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const openapi = require('./openapi')
 
-const { 
-  BUILD_PREFIX = "" 
+const {
+  BUILD_SERVER = false,
+  BUILD_PREFIX = "",
 } = process.env
 
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -10,14 +11,18 @@ const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 Promise
   .resolve({})
   .then(async () => {
+    fs.mkdirSync(__dirname + '/../docs', { recursive: true })
+    console.log('dir:    check')
+  }).then(async () => {
     let index = fs.readFileSync(__dirname + '/assets/index.html').toString()
     index = index.replace('{{BUILD_PREFIX}}', BUILD_PREFIX)
 
     fs.writeFileSync(__dirname + '/../docs/index.html', index)
     fs.writeFileSync(__dirname + '/../docs/openapi.json', JSON.stringify(openapi, null, 2))
+    console.log('assets: check')
   }).then(async () => {
-    console.log('done')
-  }).then(async () => {
-    console.log('waiting')
+    if ([true, 'true'].includes(BUILD_SERVER)) return
+
+    console.log('waiting changes...')
     await wait(60 * 60 * 1000)
   })
